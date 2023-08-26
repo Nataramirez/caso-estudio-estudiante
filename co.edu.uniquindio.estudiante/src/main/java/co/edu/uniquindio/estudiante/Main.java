663a7a63a6e15f16334f876fbe908906da6f33e3
JaviLeyton <oscarj.leytono@uqvirtual.edu.co>
2023-08-26 16:04:45 -0500

    :+1:  creacion clase estudiante

--- co.edu.uniquindio.estudiante/src/main/java/co/edu/uniquindio/estudiante/Main.java
+++ co.edu.uniquindio.estudiante/src/main/java/co/edu/uniquindio/estudiante/Main.java
@@ -1,4 +1,5 @@
 package co.edu.uniquindio.estudiante;
+import co.edu.uniquindio.estudiante.model.Estudiante;
 
 public class Main {
     public static void main(String[] args) {
---
+++ co.edu.uniquindio.estudiante/src/main/java/co/edu/uniquindio/estudiante/model/Estudiante.java
@@ -0,0 +1,67 @@
+package co.edu.uniquindio.estudiante.model;
+public class Estudiante
+{
+    private String nombre;
+    private int edad;
+    private String correo;
+    private int semestre;
+    private float nota1;
+    private float nota2;
+    private float nota3;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public int getSemestre() {
+        return semestre;
+    }
+
+    public void setSemestre(int semestre) {
+        this.semestre = semestre;
+    }
+
+    public float getNota1() {
+        return nota1;
+    }
+
+    public void setNota1(float nota1) {
+        this.nota1 = nota1;
+    }
+
+    public float getNota2() {
+        return nota2;
+    }
+
+    public void setNota2(float nota2) {
+        this.nota2 = nota2;
+    }
+
+    public float getNota3() {
+        return nota3;
+    }
+
+    public void setNota3(float nota3) {
+        this.nota3 = nota3;
+    }
+}
