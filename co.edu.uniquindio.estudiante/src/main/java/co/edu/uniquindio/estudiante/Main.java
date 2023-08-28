56d9a41a126a75cd4250bbb799c07372d4841c73
Natalia Ramírez <83674618+Nataramirez@users.noreply.github.com>
2023-08-27 19:52:23 -0500

    :construction: Ordenando codigo

--- co.edu.uniquindio.estudiante/src/main/java/co/edu/uniquindio/estudiante/Main.java
+++ co.edu.uniquindio.estudiante/src/main/java/co/edu/uniquindio/estudiante/Main.java
@@ -3,51 +3,59 @@ import co.edu.uniquindio.estudiante.model.Estudiante;
 
 public class Main {
     public static void main(String[] args) {
+        // 1. crear estudiantes
         Estudiante estudiante1 = crearEstudiante("Juan", 17, "juan@mail.com", 3, 3, 3.5, 4.5);
         Estudiante estudiante2 = crearEstudiante("Maria", 15, "pepita@mail.com", 1, 2, 2.5, 4.5);
         Estudiante estudiante3 = crearEstudiante("Pepe", 20, "pepe@mail.com", 4, 4, 2.5, 4.5);
-        calcularNotaMayorDelCurso(estudiante1, estudiante2, estudiante3);
 
-        //Calcular la definitiva de cada estudiante.
+        // 2. Calcular la definitiva de cada estudiante.
         double calcularDefinitivaEstudiante1 = calcularPromedio(estudiante1.getNota1(), estudiante1.getNota2(), estudiante1.getNota3());
         double calcularDefinitivaEstudiante2 = calcularPromedio(estudiante2.getNota1(), estudiante2.getNota2(), estudiante2.getNota3());
         double calcularDefinitivaEstudiante3 = calcularPromedio(estudiante3.getNota1(), estudiante3.getNota2(), estudiante3.getNota3());
 
-        //Calcular promedio curso.
+        // 3. Calcular promedio curso.
         double promedioCurso = calcularPromedio(calcularDefinitivaEstudiante1, calcularDefinitivaEstudiante2, calcularDefinitivaEstudiante3);
 
-        //Calcular promedio de edad.
+        // 4. Calcular promedio de edad.
         double promedioEdad = calcularPromedio(estudiante1.getEdad(), estudiante2.getEdad(), estudiante3.getEdad());
 
-        //Calcular promedio de la nota 1.
+        // 5. Calcular promedio de la nota 1.
         double promedioNota1 = calcularPromedio(estudiante1.getNota1(), estudiante2.getNota1(), estudiante3.getNota1());
 
-        //12. Nota maxima estudiantes
-        String notaMaximaEst1 = notaMaxima(estudiante1);
-        String notaMaximaEst2 = notaMaxima(estudiante2);
-        String notaMaximaEst3 = notaMaxima(estudiante3);
+        // 6. Calcular nota mayor del curso
+        double notaMayorDelCurso = calcularNotaMayorDelCurso(estudiante1, estudiante2, estudiante3);
+
+        // 7. Calcular nota menor del curso
 
         //8. Aprobacion curso
-        String aprobacionEst1= aprobacionCurso(estudiante1, calcularDefinitivaEstudiante1);
-        String aprobacionEst2= aprobacionCurso(estudiante2, calcularDefinitivaEstudiante2);
-        String aprobacionEst3= aprobacionCurso(estudiante3, calcularDefinitivaEstudiante3);
+        String aprobacionEstudiante1 = aprobacionCurso(estudiante1, calcularDefinitivaEstudiante1);
+        String aprobacionEstudiante2 = aprobacionCurso(estudiante2, calcularDefinitivaEstudiante2);
+        String aprobacionEstudiante3 = aprobacionCurso(estudiante3, calcularDefinitivaEstudiante3);
+
+        // 9. Calcular porcentaje de estudiantes que ganaron el curso
+
+        // 10. Calcular porcentaje de estudiantes que perdieron el curso
 
         //11. Obtener estudiantes con notas mayores a 4
-        String estudiantesNotaMayor4 = obtenerEstudiantesNotasMayor4(estudiante1, estudiante2, estudiante3);
+        String EstudiantesConNotasMayoresA4 = obtenerEstudiantesNotasMayor4(estudiante1, estudiante2, estudiante3);
 
-        //Imprimiendo en pantalla los resultados para su verificación.
-        System.out.println("La definitiva de " + estudiante1.getNombre() + " es " + calcularDefinitivaEstudiante1 + ", la definitiva de " +
-                estudiante2.getNombre() + " es " + calcularDefinitivaEstudiante2 + " y la definitiva de " + estudiante3.getNombre() + " es " + calcularDefinitivaEstudiante3);
-        System.out.println("El promedio del curso es " + promedioCurso);
-        System.out.println("El promedio de las edades de los estudiantes del curso es " + promedioEdad);
-        System.out.println("El promedio del curso en la nota 1 es " + promedioNota1);
-        System.out.println("Los estudiantes con una o más notas mayoes a 4 son "+estudiantesNotaMayor4);
-        System.out.println(notaMaximaEst1);
-        System.out.println(notaMaximaEst2);
-        System.out.println(notaMaximaEst3);
-        System.out.println(aprobacionEst1);
-        System.out.println(aprobacionEst2);
-        System.out.println(aprobacionEst3);
+        //12. Nota maxima estudiantes
+        String notaMaximaEstudiante1 = notaMaxima(estudiante1);
+        String notaMaximaEstudiante2 = notaMaxima(estudiante2);
+        String notaMaximaEstudiante3 = notaMaxima(estudiante3);
+    }
+
+    public static Estudiante crearEstudiante (String nombre, double edad, String correo, int semestre, double nota1,
+                                              double nota2, double nota3){
+        Estudiante estudiante = new Estudiante();
+        estudiante.setNombre(nombre);
+        estudiante.setEdad(edad);
+        estudiante.setCorreo(correo);
+        estudiante.setSemestre(semestre);
+        estudiante.setNota1(nota1);
+        estudiante.setNota2(nota2);
+        estudiante.setNota3(nota3);
+        return estudiante;
     }
 
     private static double calcularPromedio(double valorUno, double valorDos, double valorTres) {
@@ -55,10 +63,26 @@ public class Main {
         return promedioValores;
     }
 
+    public static double notaMayor(double nota1, double nota2, double nota3){
+        double notaMayor = 0;
+        if(nota1 > notaMayor) notaMayor = nota1;
+        if(nota2 > notaMayor) notaMayor = nota2;
+        if(nota3 > notaMayor) notaMayor = nota3;
+        return notaMayor;
+    }
+
+    public static double calcularNotaMayorDelCurso(Estudiante estudiante1, Estudiante estudiante2, Estudiante estudiante3){
+        double notaMayorEstudiante1 = notaMayor(estudiante1.getNota1(), estudiante1.getNota2(), estudiante1.getNota3());
+        double notaMayorEstudiante2 = notaMayor(estudiante2.getNota1(), estudiante2.getNota2(), estudiante2.getNota3());
+        double notaMayorEstudiante3 = notaMayor(estudiante3.getNota1(), estudiante3.getNota2(), estudiante3.getNota3());
+        double notaMayorCurso = notaMayor(notaMayorEstudiante1, notaMayorEstudiante2, notaMayorEstudiante3);
+        return notaMayorCurso;
+    }
+
     // 8. Aprobacion curso
     private static String aprobacionCurso(Estudiante estudiante, double promedio)
     {
-        String mensaje= String.valueOf((0));
+        String mensaje;
         if (promedio>=3.0)
         {
             mensaje=estudiante.getNombre()+" SÍ aprobó el curso";
@@ -69,39 +93,13 @@ public class Main {
         return mensaje;
     }
 
-    // 12. Nota maxima
-    private static String notaMaxima(Estudiante estudiante)
-    {
-        String mensaje;
-        double Nota1Est = estudiante.getNota1();
-        double Nota2Est = estudiante.getNota2();
-        double Nota3Est = estudiante.getNota3();
-        double NotaMaxEst = 0;
-
-        if(Nota1Est>Nota2Est)
-        {
-            NotaMaxEst=Nota1Est;
-        }
-        else
-        {
-            NotaMaxEst=Nota2Est;
-        }
-        if(Nota3Est>NotaMaxEst)
-        {
-            NotaMaxEst=Nota3Est;
-        }
-        mensaje="La nota máxima de "+estudiante.getNombre()+" es "+NotaMaxEst;
-        return mensaje;
-    }
-
-    // Nota mayor a cuadro
     public static String obtenerEstudiantesNotasMayor4(Estudiante estudiante1,
                                                        Estudiante estudiante2, Estudiante estudiante3)
     {
-        String mensaje1= String.valueOf((0));
-        String mensaje2= String.valueOf((0));
-        String mensaje3= String.valueOf((0));
-        String mensajeFinal= String.valueOf((0));
+        String mensaje1;
+        String mensaje2;
+        String mensaje3;
+        String mensajeFinal;
         double Nota1Est1=estudiante1.getNota1();
         double Nota2Est1=estudiante1.getNota2();
         double Nota3Est1=estudiante1.getNota3();
@@ -140,34 +138,29 @@ public class Main {
         mensajeFinal=mensaje1+" "+mensaje2+" "+mensaje3;
         return mensajeFinal;
     }
-    public static Estudiante crearEstudiante (String nombre, double edad, String correo, int semestre, double nota1,
-                                       double nota2, double nota3){
-        Estudiante estudiante = new Estudiante();
-        estudiante.setNombre(nombre);
-        estudiante.setEdad(edad);
-        estudiante.setCorreo(correo);
-        estudiante.setSemestre(semestre);
-        estudiante.setNota1(nota1);
-        estudiante.setNota2(nota2);
-        estudiante.setNota3(nota3);
-        return estudiante;
-    }
 
-    public static double calcularNotaMayorDelCurso(Estudiante estudiante1, Estudiante estudiante2, Estudiante estudiante3){
-        double notaMayorEstudiante1 = notaMayor(estudiante1.getNota1(), estudiante1.getNota2(), estudiante1.getNota3());
-        double notaMayorEstudiante2 = notaMayor(estudiante2.getNota1(), estudiante2.getNota2(), estudiante2.getNota3());
-        double notaMayorEstudiante3 = notaMayor(estudiante3.getNota1(), estudiante3.getNota2(), estudiante3.getNota3());
-        double notaMayorCurso = notaMayor(notaMayorEstudiante1, notaMayorEstudiante2, notaMayorEstudiante3);
-        System.out.println("La nota mayor del curso es: " + notaMayorCurso);
-        return notaMayorCurso;
-    }
+    // 12. Nota maxima
+    private static String notaMaxima(Estudiante estudiante)
+    {
+        String mensaje;
+        double Nota1Est = estudiante.getNota1();
+        double Nota2Est = estudiante.getNota2();
+        double Nota3Est = estudiante.getNota3();
+        double NotaMaxEst = 0;
 
-    public static double notaMayor(double nota1, double nota2, double nota3){
-        double notaMayor = 0;
-        if(nota1 > notaMayor) notaMayor = nota1;
-        if(nota2 > notaMayor) notaMayor = nota2;
-        if(nota3 > notaMayor) notaMayor = nota3;
-        return notaMayor;
+        if(Nota1Est>Nota2Est)
+        {
+            NotaMaxEst=Nota1Est;
+        }
+        else
+        {
+            NotaMaxEst=Nota2Est;
+        }
+        if(Nota3Est>NotaMaxEst)
+        {
+            NotaMaxEst=Nota3Est;
+        }
+        mensaje="La nota máxima de "+estudiante.getNombre()+" es "+NotaMaxEst;
+        return mensaje;
     }
-
 }
